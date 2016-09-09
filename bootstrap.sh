0b90f4125ecd473d392e71449bc397dbc2af0acc
Cindy Rubio <crubio@ucdavis.edu>
2016-09-09 12:55:40 -0700

    Using LLVM binaries.

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,28 +9,50 @@ sudo apt-get -y update
 install 'development tools' build-essential
 install 'git' git
 
-cd
-mkdir llvm
-wget -qnc http://llvm.org/releases/3.0/llvm-3.0.tar.gz
-tar xzvf llvm-3.0.tar.gz
-wget -qnc http://llvm.org/releases/3.0/clang-3.0.tar.gz
-tar xzvf clang-3.0.tar.gz
+### Updating gcc
 
-mv llvm-3.0.src llvm/src
-mv clang-3.0.src llvm/src/tools/clang
-cd llvm
-mkdir build
-cd build
-../src/configure --enable-shared
-make
+sudo apt-get install python-software-properties
 
-export LLVM_COMPILER=clang
-export LD_LIBRARY_PATH=$HOME/llvm/build/Release/lib
-export PATH=$PATH:$HOME/llvm/build/Release/bin
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+
+sudo apt-get update
+
+sudo apt-get install gcc-4.8
+
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 50
+
+
+### Updating g++
+
+sudo apt-get install g++-4.8
+
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 50
+
+
+### Downloading llvm 3.0 binaries
+
+cd $HOME
+
+wget http://llvm.org/releases/3.0/clang+llvm-3.0-x86_64-linux-Ubuntu-11_10.tar.gz
+
+tar -xzvf clang+llvm-3.0-x86_64-linux-Ubuntu-11_10.tar.gz
+
+mv clang+llvm-3.0-x86_64-linux-Ubuntu-11_10 llvm-3.0
+
+rm -f clang+llvm-3.0-x86_64-linux-Ubuntu-11_10.tar
+
+
+### Updating .bashrc file
+
+echo "export PATH=$HOME/llvm-3.0/bin:$PATH" >> ~/.bashrc
+echo "export LD_LIBRARY_PATH=$HOME/llvm-3.0/lib:$LD_LIBRARY_PATH" >> ~/.bashrc
+
+export PATH=$HOME/llvm-3.0/bin:$PATH
+export LD_LIBRARY_PATH=$HOME/llvm-3.0/lib:$LD_LIBRARY_PATH
 
 echo "export LLVM_COMPILER=clang" >> ~/.bashrc
-echo "export LD_LIBRARY_PATH=$HOME/llvm/build/Release/lib" >> ~/.bashrc
-echo "export PATH=$PATH:$HOME/llvm/build/Release/bin" >> ~/.bashrc
+
+### Installing scons
 
 cd
 wget -qnc http://prdownloads.sourceforge.net/scons/scons-2.4.0.tar.gz
@@ -39,16 +61,20 @@ mv scons-2.4.0 scons
 cd scons
 sudo python2.7 setup.py install
 
+### Cloning Precimonious
+
 cd
 git clone https://github.com/corvette-berkeley/precimonious.git
 cd precimonious
 
-export CORVETTE_PATH=$HOME/precimonious
 echo "export CORVETTE_PATH=$HOME/precimonious" >> ~/.bashrc
+
+source ~/.bashrc
+
 
 cd src
 sed -i "s/SHLINKFLAGS='-Wl',/SHLINKFLAGS='',/g" SConscript
-scons -Uc
+sed -i "s/LIBS='LLVM-\$llvm_version'/#LIBS='LLVM-\$llvm_version'/g" SConscript
 scons -U
 scons -U test
 
